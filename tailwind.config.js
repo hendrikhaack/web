a107ac4c9c222956c9f86915c86025949bc9c689
Hendrik Haack <hendrik@einsinn.de>
2021-07-04 14:33:22 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  purge: [ './index.html' ],
+  darkMode: 'media',
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#FFF',
+      gray: {
+        50: '#FAFAFA',
+        100: '#F5F5F5',
+        200: '#E5E5E5',
+        300: '#D4D4D4',
+        400: '#A3A3A3',
+        500: '#737373',
+        600: '#525252',
+        700: '#404040',
+        800: '#262626',
+        900: '#171717',
+      },
+    },
+    extend: {
+      animation: {
+        'waving': 'waving 2.5s ease infinite',
+      },
+      borderColor: (theme) => ({
+        DEFAULT: theme('colors.gray.300', 'currentColor'),
+      }),
+      keyframes: {
+        waving: {
+          '0%, 100%': { transform: 'rotate(15deg)' },
+          '50%': { transform: 'rotate(-15deg)' },
+          '75%': { transform: 'rotate(15deg)' },
+        },
+      },
+      zIndex: {
+        '-1': '-1',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      opacity: ['group-focus-visible'],
+      ringWidth: ['focus-visible', 'group-focus-visible'],
+      textColor: ['can-hover'],
+      translate: ['group-hover', 'group-focus-visible'],
+      width: ['group-hover'],
+    },
+  },
+  plugins: [
+    require('tailwindcss-interaction-variants'),
+  ],
+};
